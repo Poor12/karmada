e0a583808526e2c6c5ac0d6a9cd9c4c980df1e4e
changzhen <changzhen5@huawei.com>
2023-01-20 10:38:07 +0800

    tmp
    Signed-off-by: changzhen <changzhen5@huawei.com>

--- test/e2e/propagatequantity_test.go
+++ test/e2e/propagatequantity_test.go
@@ -3,11 +3,14 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
 
 	policyv1alpha1 "github.com/karmada-io/karmada/pkg/apis/policy/v1alpha1"
 	"github.com/karmada-io/karmada/test/e2e/framework"
@@ -15,14 +18,15 @@ import (
 )
 
 // BasicPropagation focus on basic propagation functionality testing.
-var _ = ginkgo.Describe("propagation quantity testing", func() {
+var _ = ginkgo.Describe("namespace scope resource propagation quantity testing", func() {
 	var deploymentSlice []*appsv1.Deployment
 	var policySlice []*policyv1alpha1.PropagationPolicy
 	var quantityNum int
 	var targetClusters []string
+	var startTime, rbTime, workTime, finishTime time.Time
 
 	ginkgo.BeforeEach(func() {
-		quantityNum = 10
+		quantityNum = 1000
 		targetClusters = []string{"member1"}
 
 		policySlice = make([]*policyv1alpha1.PropagationPolicy, quantityNum)
@@ -50,20 +54,51 @@ var _ = ginkgo.Describe("propagation quantity testing", func() {
 	})
 
 	ginkgo.BeforeEach(func() {
+		startTime = time.Now()
+		for index := 0; index < quantityNum; index++ {
+			go func(index int) {
+				deployment := deploymentSlice[index]
+				policy := policySlice[index]
+
+				framework.CreatePropagationPolicy(karmadaClient, policy)
+				framework.CreateDeployment(kubeClient, deployment)
+			}(index)
+		}
+	})
+
+	ginkgo.AfterEach(func() {
+		fmt.Printf("rb time: %f\n", rbTime.Sub(startTime).Minutes())
+		fmt.Printf("work time: %f\n", workTime.Sub(rbTime).Minutes())
+		fmt.Printf("finish time: %f\n", finishTime.Sub(startTime).Minutes())
+
 		for index := 0; index < quantityNum; index++ {
 			deployment := deploymentSlice[index]
 			policy := policySlice[index]
-
-			framework.CreatePropagationPolicy(karmadaClient, policy)
-			framework.CreateDeployment(kubeClient, deployment)
-			ginkgo.DeferCleanup(func() {
-				framework.RemovePropagationPolicy(karmadaClient, policy.Namespace, policy.Name)
-				framework.RemoveDeployment(kubeClient, deployment.Namespace, deployment.Name)
-			})
+			framework.RemovePropagationPolicy(karmadaClient, policy.Namespace, policy.Name)
+			framework.RemoveDeployment(kubeClient, deployment.Namespace, deployment.Name)
 		}
 	})
 
 	ginkgo.It("deployment propagation testing", func() {
+		gomega.Eventually(func() bool {
+			rbs, err := karmadaClient.WorkV1alpha2().ResourceBindings(testNamespace).List(context.TODO(), metav1.ListOptions{})
+			if err != nil {
+				return false
+			}
+
+			return len(rbs.Items) == quantityNum
+		}, pollTimeout, pollInterval).Should(gomega.Equal(true))
+		rbTime = time.Now()
+
+		gomega.Eventually(func() bool {
+			works, err := karmadaClient.WorkV1alpha1().Works("karmada-es-member1").List(context.TODO(), metav1.ListOptions{})
+			if err != nil {
+				return false
+			}
+
+			return len(works.Items) > quantityNum
+		}, pollTimeout, pollInterval).Should(gomega.Equal(true))
+		workTime = time.Now()
 
 		for _, cluster := range targetClusters {
 			clusterClient := framework.GetClusterClient(cluster)
@@ -78,13 +113,56 @@ var _ = ginkgo.Describe("propagation quantity testing", func() {
 				return len(deploys.Items) == quantityNum
 			}, pollTimeout, pollInterval).Should(gomega.Equal(true))
 		}
-
-		// for index := 0; index < quantityNum; index++ {
-		// 	deployment := deploymentSlice[index]
-		// 	framework.WaitDeploymentPresentOnClustersFitWith(targetClusters, deployment.Namespace, deployment.Name,
-		// 		func(deployment *appsv1.Deployment) bool {
-		// 			return true
-		// 		})
-		// }
+		finishTime = time.Now()
 	})
+})
+
+var _ = ginkgo.Describe("cluster scope resource propagation quantity testing", func() {
+	ginkgo.Context("CustomResourceDefinition propagation testing", func() {
+		var crdGroup string
+		var randStr string
+		var crdSpecNames apiextensionsv1.CustomResourceDefinitionNames
+		var crd *apiextensionsv1.CustomResourceDefinition
+		var crdPolicy *policyv1alpha1.ClusterPropagationPolicy
+
+		ginkgo.BeforeEach(func() {
+			crdGroup = fmt.Sprintf("example-%s.karmada.io", rand.String(RandomStrLength))
+			randStr = rand.String(RandomStrLength)
+			crdSpecNames = apiextensionsv1.CustomResourceDefinitionNames{
+				Kind:     fmt.Sprintf("Foo%s", randStr),
+				ListKind: fmt.Sprintf("Foo%sList", randStr),
+				Plural:   fmt.Sprintf("foo%ss", randStr),
+				Singular: fmt.Sprintf("foo%s", randStr),
+			}
+			crd = testhelper.NewCustomResourceDefinition(crdGroup, crdSpecNames, apiextensionsv1.NamespaceScoped)
+			crdPolicy = testhelper.NewClusterPropagationPolicy(crd.Name, []policyv1alpha1.ResourceSelector{
+				{
+					APIVersion: crd.APIVersion,
+					Kind:       crd.Kind,
+					Name:       crd.Name,
+				},
+			}, policyv1alpha1.Placement{
+				ClusterAffinity: &policyv1alpha1.ClusterAffinity{
+					ClusterNames: framework.ClusterNames(),
+				},
+			})
+		})
+
+		ginkgo.BeforeEach(func() {
+			framework.CreateClusterPropagationPolicy(karmadaClient, crdPolicy)
+			framework.CreateCRD(dynamicClient, crd)
+			ginkgo.DeferCleanup(func() {
+				framework.RemoveClusterPropagationPolicy(karmadaClient, crdPolicy.Name)
+				framework.RemoveCRD(dynamicClient, crd.Name)
+				framework.WaitCRDDisappearedOnClusters(framework.ClusterNames(), crd.Name)
+			})
+		})
+
+		ginkgo.It("crd propagation testing", func() {
+			framework.GetCRD(dynamicClient, crd.Name)
+			framework.WaitCRDPresentOnClusters(karmadaClient, framework.ClusterNames(),
+				fmt.Sprintf("%s/%s", crd.Spec.Group, "v1alpha1"), crd.Spec.Names.Kind)
+		})
+	})
+
 })
